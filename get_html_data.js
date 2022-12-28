fa8228c5ddc2f732d2da04561d0c6e02b6cdf190
arctic-cheetah <gondonlondon@gmail.com>
2022-12-28 12:40:17 +1100

    update fetch from website

--- get_html_data.js
+++ get_html_data.js
@@ -1,9 +1,12 @@
+import fetch from 'node-fetch';
+
 function onReceive() {
     console.log(this.responseText);
 }
-let req = new XMLHttpRequest();
-let url = ["https://incidecoder.com/products/frudia-green-grape-sebum-control-cooling-sun-gel"];
 
-req.addEventListener("load", onReceive);
-req.open("GET", url[0]);
-req.send();
+let url = ["https://incidecoder.com/products/frudia-green-grape-sebum-control-cooling-sun-gel"];
+const response = await fetch(url);
+const body = await response.text();
+
+
+console.log(body);

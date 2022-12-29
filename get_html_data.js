06dcc52e88c37424822557bfa6315e031c9b9af5
arctic-cheetah <gondonlondon@gmail.com>
2022-12-29 12:50:59 +1100

    Refactorised code, organised data using objects

--- get_html_data.js
+++ get_html_data.js
@@ -9,7 +9,6 @@ let url = ["https://incidecoder.com/products/frudia-green-grape-sebum-control-co
 "https://incidecoder.com/products/frudia-avocado-greenery-relief-sun-cream"];
 
 
-
 // Function to fetch html from website
 async function fetch_data(url) {
     let data = [];
@@ -21,6 +20,8 @@ async function fetch_data(url) {
 }
 
 // Function to convert DOM to text
+// Assumes parameters are an array
+// Return array of text
 function dom_to_text (page) {
     let dat = [];
     for (let i = 0; i < page.length; i+=1) {
@@ -32,7 +33,7 @@ function dom_to_text (page) {
     
 }
 
-// Extract ingredients using regex
+// Extract ingredients into text form using regex 
 function get_ingredients (data) {
     let reg = /(?<=>)([\w ,\-_]+)(?=<)/g;
     let ingredients = [];
@@ -50,5 +51,22 @@ let page = await fetch_data(url);
 let temp = dom_to_text(page);
 let sunblock_ingred = get_ingredients(temp);
 
-console.log(sunblock_ingred);
+//Key-value object to associate an ingredient with the type of sunblock 
+let sunblock = new Object();
+sunblock.green_grape = sunblock_ingred[0];
+sunblock.ultra_uv = sunblock_ingred[1];
+sunblock.avocado = sunblock_ingred[2];
+// console.log(sunblock.green_grape);
+
+
+
+//Find the difference in ingredient
+let diff_uv_grape = sunblock.green_grape.filter(e_grape => (sunblock.ultra_uv.find(e_uv => e_uv == e_grape) == undefined ? 1 : 0));
+console.log(diff_uv_grape);
+
+let diff_grape_avo = sunblock.avocado.filter(e_avo=> (sunblock.green_grape.find(e_grape => e_grape == e_avo) == undefined ? 1 : 0));
+console.log(diff_grape_avo);
+
+let diff_uv_avo = sunblock.ultra_uv.filter(e_uv=> (sunblock.avocado.find(e_avo => e_uv == e_avo) == undefined ? 1 : 0));
+console.log(diff_uv_avo);
 
